d25b6a1f6871738cb2012eb8622206c6b978aaee
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-03 15:41:44 +0600

    Вывел вызов всех компонентов в одном компоненте

--- src/containers/Mamba/Mamba.tsx
+++ src/containers/Mamba/Mamba.tsx
@@ -1,16 +1,30 @@
 import Navbar from '../../components/Navbar/Navbar.tsx';
 import UsersForm from '../../components/UsersForm/UsersForm.tsx';
+import Users from '../../components/Users/Users.tsx';
+import { useState } from 'react';
+import { IUsers } from '../../types';
 
 const Mamba = () => {
+  const [users, setUsers] = useState<IUsers[]>([]);
+
+  const addNewUser = (newUser: IUsers): void => {
+    setUsers((prevState) => [...prevState, newUser]);
+  }
+
   return (
     <>
       <header>
         <Navbar/>
       </header>
-      <main className='container'>
-        <div className='row'>
-          <div className='col-4'>
-            <UsersForm/>
+      <main className="container">
+        <div className="row">
+          <div className="col-4 mb-2">
+            <UsersForm addNewUser={addNewUser}/>
+          </div>
+          <div className="col-lg-8 mb-2">
+            <div className="row">
+              <Users users={users}/>
+            </div>
           </div>
         </div>
       </main>
