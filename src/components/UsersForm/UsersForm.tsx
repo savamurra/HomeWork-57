0e5bb5c726cf7bf618f27858e9cb2bd12fc0e6b6
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-03 15:37:37 +0600

    Добавил логику для создания новых пользователей. С помощью метода changeUser отслеживаю вводимые данные в форме а также немного изменил код для проверки чекбокса для отслеживания состояния статуса

--- src/components/UsersForm/UsersForm.tsx
+++ src/components/UsersForm/UsersForm.tsx
@@ -1,11 +1,63 @@
+import { useState } from 'react';
+import { IUser, IUsers } from '../../types';
+import * as React from 'react';
+interface IUsersFormProps {
+  addNewUser: (newUser: IUsers) => void;
+}
 
-const UsersForm = () => {
+const UsersForm:React.FC<IUsersFormProps> = ({addNewUser}) => {
+
+  const role: { userRole: string }[] = [
+    {userRole: 'Admin'},
+    {userRole: 'Employee'},
+    {userRole: 'Consumer',}
+  ]
+
+
+    const [newUser, setNewUser] = useState<IUser>({
+      name: '',
+      email: '',
+      activity: false,
+      role: '',
+      urlImage: '',
+    })
+
+
+  const changeUser = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setNewUser((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.type === "checkbox" ? e.target.checked : e.target.value,
+      }
+    })
+  }
+
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    addNewUser({
+      id: crypto.randomUUID(),
+      ...newUser,
+    });
+
+    setNewUser({
+      name: "",
+      email: "",
+      activity: false,
+      role:"",
+      urlImage: ""
+    })
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h3>Add new Users</h3>
       <div className="form-group mb-2">
         <label htmlFor="name">Name:</label>
         <input
+          onChange={changeUser}
+          value={newUser.name}
           type="text"
           id="name"
           name="name"
@@ -16,6 +68,8 @@ const UsersForm = () => {
       <div className="form-group mb-4">
         <label htmlFor="email">Email:</label>
         <input
+          onChange={changeUser}
+          value={newUser.email}
           type="email"
           id="email"
           name="email"
@@ -23,8 +77,22 @@ const UsersForm = () => {
         />
       </div>
 
+      <div className="form-group mb-4">
+        <label htmlFor="urlImage">Url image:</label>
+        <input
+          value={newUser.urlImage}
+          onChange={changeUser}
+          type="url"
+          id="urlImage"
+          name="urlImage"
+          className="form-control"
+        />
+      </div>
+
       <div className="btn-group mb-3" role="group" aria-label="Basic checkbox toggle button group">
         <input
+          onChange={changeUser}
+          checked={newUser.activity}
           type="checkbox"
           className="btn-check"
           id="activity"
@@ -34,22 +102,27 @@ const UsersForm = () => {
           htmlFor="activity"
           className="btn btn-outline-primary"
         >
-          Activity:
+          Activity
         </label>
       </div>
 
-      <div className="form-floating">
+      <div className="form-floating mb-3">
         Role:
-        <label htmlFor="role"></label>
+
         <select
-          className="form-select"
+          className="form-select p-2"
           name="role"
           id="role"
+          value={newUser.role}
+          onChange={changeUser}
         >
           <option value="" disabled>Выберите Роль</option>
+          {role.map((item) => (
+            <option key={item.userRole + 1} value={item.userRole}>{item.userRole}</option>
+          ))}
         </select>
       </div>
-
+      <button className="btn btn-primary">Add</button>
     </form>
   );
 };
