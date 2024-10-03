01b241b31a4b433564381fe70c49292fe29b0686
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-03 15:40:53 +0600

    Нарисовал карточку пользователя для оторбражения данных

--- src/components/Users/UsersItem.tsx
+++ src/components/Users/UsersItem.tsx
@@ -1,7 +1,22 @@
-const UsersItem = () => {
-  return (
-    <div>
+import { IUsers } from '../../types';
+import * as React from 'react';
 
+interface Props {
+  user: IUsers
+}
+
+const UsersItem:React.FC<Props> = ({user}) => {
+  const imageUrl = `${user.urlImage}` || "https://i.pinimg.com/474x/59/af/9c/59af9cd100daf9aa154cc753dd58316d.jpg"
+
+  return (
+    <div className="card p-2 m-2" style={{width: '230px'}}>
+      <img src={imageUrl} className="card-img-top" alt="UserImg" style={{height: '200px'}}/>
+      <div className="card-body">
+        <p className="card-title">Name: <strong>{user.name}</strong></p>
+        <p className="card-text">Email: <strong>{user.email}</strong></p>
+        <p className="card-text">Status: <strong>{user.activity ? 'Активен' : 'Бездействует'}</strong></p>
+        <p className="card-text">Role: <strong>{user.role}</strong></p>
+      </div>
     </div>
   );
 };
